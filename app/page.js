a7162f1ca1fc4d481b92362a4bf6d32024050372
Akshat Mishra <akshatmishra317@gmail.com>
2024-06-22 22:31:16 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,215 @@
-import Image from "next/image";
+"use client"
+import { useRef, useState } from 'react';
+import { useJsApiLoader, GoogleMap, MarkerF, Autocomplete, DirectionsRenderer } from '@react-google-maps/api';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import button from './assets/location-pin.png';
+import originIcon from './assets/OriginIcon.png';
+import destinationIcon from './assets/DestinationIcon.png';
+import GravitiLogo from './assets/Graviti-Logo.png';
+import Image from 'next/image';
 
 export default function Home() {
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [directionsRes, setDirectionsRes] = useState(null);
+  const [distance, setDistance] = useState('');
+  const [time, setTime] = useState('');
+  const [routes, setRoutes] = useState([]);
+  const [waypoints, setWaypoints] = useState([]);
+
+  const [originloc, setOriginLoc] = useState({ lat: 0, lng: 0 });
+  const [destinationloc, setDestinationLoc] = useState({ lat: 0, lng: 0 });
+
+  const originRef = useRef();
+  const destinationRef = useRef();
+  const waypointRef = useRef();
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+    libraries: ['places'],
+  });
+
+  const center = { lat: 28.6567, lng: 77.2415 };
+
+  if (!isLoaded) {
+    return <p>Loading ...</p>;
+  }
+
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destinationRef.current.value === '') {
+      return;
+    }
+    setLoading(true);
+    const directionService = new window.google.maps.DirectionsService();
+    const routeOptions = {
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      waypoints: [...waypoints.map((waypoint) => ({ location: waypoint.location }))],
+      travelMode: window.google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true,
+    };
+    const results = await new Promise((resolve, reject) => {
+      directionService.route(routeOptions, (response, status) => {
+        if (status === 'OK') {
+          resolve(response);
+        } else {
+          reject(status);
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    });
+
+    setOrigin(originRef.current.value);
+    setDestination(destinationRef.current.value);
+    setLoading(false);
+    setDirectionsRes(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setTime(results.routes[0].legs[0].duration.text);
+    setRoutes(results.routes);
+
+    setOriginLoc(results.routes[0].legs[0].start_location);
+    setDestinationLoc(results.routes[0].legs[0].end_location);
+  }
+
+  const handleAddWaypoint = () => {
+    const newWaypoint = { location: waypointRef.current.value };
+    setWaypoints((prevWaypoints) => [...prevWaypoints, newWaypoint]);
+    waypointRef.current.value = '';
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    
+    <>
+    <nav className='bg-white hidden sm:flex'>
+        <Image src={GravitiLogo} alt='logo' className='ml-4 my-2'/>
+      </nav>
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      
+      <div className="container flex flex-col items-center">
+        <p className="text-xl text-blue-900 my-8">Let's calculate <span className='font-bold'>distance</span> from Google maps</p>
+        <div className="flex flex-col-reverse sm:flex-row w-full items-center justify-around">
+          <div className="flex flex-col space-y-4 w-full ml-0 md:ml-16 md:w-5/9 p-4">
+           <div className='flex flex-col sm:flex-row'>
+           <div className='flex flex-col'>
+           <div>
+              <label className="block text-sm font-medium text-gray-700">Origin</label>
+              <Autocomplete>
+                <input
+                  type="text"
+                  placeholder="Origin"
+                  ref={originRef}
+                  className="mt-1 block w-full sm:w-250 p-2 border text-gray-900 border-gray-300 rounded-md"
+                />
+              </Autocomplete>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Stop</label>
+              <Autocomplete>
+                <input
+                  type="text"
+                  placeholder="Waypoints"
+                  ref={waypointRef}
+                  className="mt-1 block w-full sm:w-250 p-2 border border-gray-300 text-gray-900 rounded-md"
+                />
+              </Autocomplete>
+              <button className="flex items-center justify-items-end mt-2 text-gray-500" onClick={handleAddWaypoint}>
+                <AiOutlinePlusCircle className="mr-1" /> Add another stop
+              </button>
+            </div>
+            <div className="space-y-2">
+              {waypoints.map((waypoint, index) => (
+                <div key={index} className="text-blue-900">
+                  {waypoint.location}
+                </div>
+              ))}
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Destination</label>
+              <Autocomplete>
+                <input
+                  type="text"
+                  placeholder="Destination"
+                  ref={destinationRef}
+                  className="mt-1 block w-full sm:w-250 p-2 border border-gray-300 text-gray-900 rounded-md"
+                />
+              </Autocomplete>
+            </div>
+            
+           </div>
+           <div className='flex justify-center items-center mt-4 sm:m-auto'>
+           <button className="bg-blue-800 text-white p-4 rounded-full" onClick={calculateRoute}>
+              Calculate
+            </button>
+           </div>
+           </div>
+            
+            
+            <div className="bg-white p-4 flex justify-between">
+                <div className="text-lg font-semibold text-gray-900">Distance</div>
+                <div className="text-2xl text-blue-400">{distance}</div>
+            </div>
+            <div className='p-4 mt-0'>
+              <div className="text-black">
+                {!loading ? (
+                  <p>
+                    The distance between {origin} and {destination} via the selected route is {distance}.
+                  </p>
+                ) : (
+                  <p>Please select a route.</p>
+                )}
+                {loading && <div className="text-blue-500">Loading...</div>}
+              </div>
+            </div>
+          </div>
+          <div className="w-full sm:w-560 h-96 mt-8 md:mt-0">
+            <GoogleMap
+              center={center}
+              zoom={12}
+              mapContainerStyle={{ width: '100%', height: '100%' }}
+              options={{
+                streetViewControl: false,
+                mapTypeControl: false,
+                fullscreenControl: false,
+                disableDefaultUI: true,
+              }}
+            >
+              <MarkerF
+                position={center}
+                clickable={false}
+                icon={{
+                  url: button,
+                  scaledSize: new window.google.maps.Size(30, 30),
+                }}
+              />
+              <MarkerF
+                position={originloc}
+                clickable={false}
+                icon={{
+                  url: originIcon,
+                  scaledSize: new window.google.maps.Size(20, 20),
+                }}
+                zIndex={10}
+                draggable={true}
+                onDragEnd={(e) => setOriginLoc({ lat: e.latLng.lat(), lng: e.latLng.lng() })}
+              />
+              <MarkerF
+                position={destinationloc}
+                clickable={false}
+                icon={{
+                  url: destinationIcon,
+                  scaledSize: new window.google.maps.Size(20, 20),
+                }}
+                zIndex={10}
+                draggable={true}
+                onDragEnd={(e) => setDestinationLoc({ lat: e.latLng.lat(), lng: e.latLng.lng() })}
+              />
+              {directionsRes && <DirectionsRenderer directions={directionsRes} />}
+            </GoogleMap>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    </>
   );
 }
